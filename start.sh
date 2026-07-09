302e22e821eb5104949a545d4d02371f7fccd129
t0twn <t.t.w.n@outlook.com>
2026-07-10 00:13:29 +0800

    use exec in start script

--- start.sh
+++ start.sh
@@ -7,21 +7,25 @@ if [ $# -lt 1 ]; then
     exit 1
 fi
 
-to_lower_dash() { echo $1 | tr 'A-Z' 'a-z' | tr '-' '_'; }
+to_lower_dash() { echo "$1" | tr 'A-Z' 'a-z' | tr '-' '_'; }
 
 CMD="$1"
-ENV_PREFIX="$(to_lower_dash $CMD)_"
+ENV_PREFIX="$(to_lower_dash "$CMD")_"
 shift
 
 ARGS=""
 # Collect matching environment variables
 while IFS='=' read -r name value; do
-    if [[ "$(to_lower_dash $name)" == ${ENV_PREFIX}* ]]; then
-        arg_name="${name#$ENV_PREFIX}"           # Remove prefix
-        arg_name="$(to_lower_dash $arg_name)"    # To lowercase dash
-        ARGS="$ARGS --$arg_name $value"
-    fi
-done < <(env)
+    normalized_name="$(to_lower_dash "$name")"
+    case "$normalized_name" in
+        ${ENV_PREFIX}*)
+            arg_name="${normalized_name#$ENV_PREFIX}"
+            ARGS="$ARGS --$arg_name $value"
+            ;;
+    esac
+done <<EOF
+$(env)
+EOF
 
 # Run the command with arguments
-eval "$CMD $ARGS $*"
+exec "$CMD" $ARGS "$@"
